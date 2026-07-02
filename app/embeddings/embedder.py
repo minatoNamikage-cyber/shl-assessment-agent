7ebab7b5de08194c97bada36c9b165f49cf59969
minatoNamikage-cyber <siddharthgautam0000@gmail.com>
2026-07-02 14:03:09 +0530

    Update embedder.py

--- app/embeddings/embedder.py
+++ app/embeddings/embedder.py
@@ -1,26 +1,48 @@
-from sentence_transformers import SentenceTransformer
+import requests
+import numpy as np
+from app.core.config import settings
 
 
 class EmbeddingService:
 
     def __init__(self):
-
-        self.model = SentenceTransformer(
-            "BAAI/bge-small-en-v1.5"
+        self.url = (
+            "https://router.huggingface.co/"
+            "hf-inference/models/BAAI/bge-small-en-v1.5"
         )
+
+        self.headers = {
+            "Authorization": f"Bearer {settings.HF_TOKEN}"
+        }
 
     def encode(self, texts):
 
         if isinstance(texts, str):
             texts = [texts]
 
-        embeddings = self.model.encode(
-            texts,
-            normalize_embeddings=True,
-            convert_to_numpy=True
-        )
+        embeddings = []
 
-        return embeddings
+        for text in texts:
+
+            response = requests.post(
+                self.url,
+                headers=self.headers,
+                json={
+                    "inputs": text
+                },
+                timeout=60
+            )
+
+            response.raise_for_status()
+
+            vector = response.json()
+
+            embeddings.append(vector)
+
+        return np.array(
+            embeddings,
+            dtype=np.float32
+        )
 
 
 embedding_service = EmbeddingService()
